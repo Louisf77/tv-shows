a96e5eb1ff32bac8f057ca4f0338b79aa2fd8a1b
Louis Freeman <louis.freeman@gmail.com>
2021-06-24 16:14:01 +0100

    add css into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import episodes from "./episodes.json";
 import Episodes from "./episodes-content";
+import "./App.css"
 
 console.log(`Imported ${episodes.length} episode(s)`);
 console.log(`First episode's name is ${episodes[0].name}`);
 
 function App(): JSX.Element {
   return (
-    <>
+    <main>
       <Episodes />
-    </>
+    </main>
   );
 }
 
