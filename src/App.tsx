d26058c1654026b0abf4fdfc2ebf94299fc2dfd8
Louis Freeman <louis.freeman@gmail.com>
2021-06-25 14:15:00 +0100

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import episodes from "./episodes.json";
 import Episodes from "./episodes-content";
 import "./App.css";
+import Header from "./header"
 
 console.log(`Imported ${episodes.length} episode(s)`);
 console.log(`First episode's name is ${episodes[0].name}`);
 
 function App(): JSX.Element {
   return (
+    <div className = 'background'>
+    <Header />
     <main>
+      <div className = 'cards'>
       <Episodes />
+      </div>
     </main>
+    </div>
   );
 }
 
