7094d377f41b2daf2f35402a16a13d8422ac7ace
Louis Freeman <louis.freeman@gmail.com>
2021-07-01 14:34:31 +0100

    add search bar

--- src/episodes-content.tsx
+++ src/episodes-content.tsx
@@ -1,28 +1,43 @@
 import episodes from "./episodes.json";
 import EpisodesStructure from "./episodes-structure";
+import { useState } from "react";
 
 function Episodes(): JSX.Element {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
-    <div className="cards">
-      {episodes.map((episodeObject) => (
-        <EpisodesStructure
-          key={episodeObject.id}
-          id={episodeObject.id}
-          url={episodeObject.url}
-          name={episodeObject.name}
-          season={episodeObject.season}
-          number={episodeObject.number}
-          type={episodeObject.type}
-          airdate={episodeObject.airdate}
-          airtime={episodeObject.airtime}
-          airstamp={episodeObject.airstamp}
-          runtime={episodeObject.runtime}
-          image={episodeObject.image}
-          summary={episodeObject.summary}
-          _links={episodeObject._links}
-        />
-      ))}
-    </div>
+    <>
+      <input
+        value={searchTerm}
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
+
+      <div className="cards">
+        {episodes
+          .filter((episode) =>
+            episode.name.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .map((episodeObject) => (
+            <EpisodesStructure
+              key={episodeObject.id}
+              id={episodeObject.id}
+              url={episodeObject.url}
+              name={episodeObject.name}
+              season={episodeObject.season}
+              number={episodeObject.number}
+              type={episodeObject.type}
+              airdate={episodeObject.airdate}
+              airtime={episodeObject.airtime}
+              airstamp={episodeObject.airstamp}
+              runtime={episodeObject.runtime}
+              image={episodeObject.image}
+              summary={episodeObject.summary}
+              _links={episodeObject._links}
+            />
+          ))}
+      </div>
+    </>
   );
 }
 
