296ab64785987ddd7e84ac329ed9401ef5653c00
Louis Freeman <louis.freeman@gmail.com>
2021-07-08 14:23:03 +0100

    add show selector to full content

--- src/components/fullcontent.tsx
+++ src/components/fullcontent.tsx
@@ -3,24 +3,30 @@ import Dropdown from "./dropdown";
 import SearchBar from "./searchbar";
 import Episodes from "./episodefilter";
 import { IEpisode } from "../utils/Types";
+import ShowSelector from "./showselector";
 
 function FullContent(): JSX.Element {
   const [searchTerm, setSearchTerm] = useState("");
   const [dropTerm, setDropTerm] = useState("");
   const [episodes, setEpisodes] = useState<IEpisode[]>([]);
+  const [show,setShow] = useState<number>(172)
   useEffect(() => {
     const getEpisodes = async () => {
-      const res = await fetch(`https://api.tvmaze.com/shows/172/episodes`);
+      const res = await fetch(`https://api.tvmaze.com/shows/${show}/episodes`);
       const jsonBody = await res.json();
       setEpisodes(jsonBody);
       console.log("test");
     };
 
     getEpisodes();
-  }, []);
+  }, [show]);
   return (
     <>
       <div className="navbar">
+        <ShowSelector 
+          show={show}
+          setShow={setShow}
+          />
         <Dropdown
           episodes={episodes}
           dropTerm={dropTerm}
